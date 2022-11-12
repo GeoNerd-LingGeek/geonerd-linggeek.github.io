4ef13966a744a1cd32e1df02d0bbe88c8d28054d
GeoNerd LingGeek <115885082+GeoNerd-LingGeek@users.noreply.github.com>
2022-11-13 00:20:33 +0900

    Update script.js
    bugfix: languages tab still wouldn't be highlighted

--- script.js
+++ script.js
@@ -3,23 +3,28 @@ function sidebar_button_highlight() {
         document.getElementById("home").style.backgroundColor = "rgb(90,101,234)";
         document.getElementById("home").style.borderRadius = "37%";
         document.getElementById("home").style.transition = "ease-in-out 0.2s";
-    } else if (window.location.href === "https://geonerd-linggeek.github.io/games/") {
+    }
+    if (window.location.href === "https://geonerd-linggeek.github.io/games/") {
         document.getElementById("games").style.backgroundColor = "rgb(90,101,234)";
         document.getElementById("games").style.borderRadius = "37%";
         document.getElementById("games").style.transition = "ease-in-out 0.2s";
-    } else if (window.location.href === "https://geonerd-linggeek.github.io/bots/") {
+    }
+    if (window.location.href === "https://geonerd-linggeek.github.io/bots/") {
         document.getElementById("bots").style.backgroundColor = "rgb(90,101,234)";
         document.getElementById("bots").style.borderRadius = "37%";
         document.getElementById("bots").style.transition = "ease-in-out 0.2s";
-    } else if (window.location.href === "https://geonerd-linggeek.github.io/about/") {
+    }
+    if (window.location.href === "https://geonerd-linggeek.github.io/about/") {
         document.getElementById("about").style.backgroundColor = "rgb(90,101,234)";
         document.getElementById("about").style.borderRadius = "37%";
         document.getElementById("about").style.transition = "ease-in-out 0.2s";
-    } else if (window.location.href === "https://geonerd-linggeek.github.io/channels/") {
+    }
+    if (window.location.href === "https://geonerd-linggeek.github.io/channels/") {
         document.getElementById("channels").style.backgroundColor = "rgb(90,101,234)";
         document.getElementById("channels").style.borderRadius = "37%";
         document.getElementById("channels").style.transition = "ease-in-out 0.2s";
-    } else if (window.location.href === "https://geonerd-linggeek.github.io/languages/") {
+    }
+    if (window.location.href === "https://geonerd-linggeek.github.io/languages/") {
         document.getElementById("languages").style.backgroundColor = "rgb(90,101,234)";
         document.getElementById("languages").style.borderRadius = "37%";
         document.getElementById("languages").style.transition = "ease-in-out 0.2s";
